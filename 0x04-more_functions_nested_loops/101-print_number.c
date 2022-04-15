3fc88a1305bf9969dfab0492bfbccbd7b176b2ec
Brian Onyango <95346886+Brian-Onyango@users.noreply.github.com>
2022-04-15 04:55:19 -0700

    updated

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,37 +1,22 @@
 #include "main.h"
+
 /**
- * print_number - prints numbers
- * @n: number to be printed
- * Return:void
+ * print_number - prints an integer
+ * @n: tracked var
  */
 
 void print_number(int n)
 {
-
-	int d = 1, i = 0, ii = 0;
+	unsigned int x = n;
 
 	if (n < 0)
 	{
-		_putchar('-');
-		n = -n;
+		_putchar(45);
+		x = -x;
 	}
-
-	while (n / d != 0)
+	if ((x / 10) > 0)
 	{
-		d *= 10;
-		i++;
+		print_number(x / 10);
 	}
-	d = d / 10;
-
-	while (ii < i)
-	{
-		_putchar('0' + n / d);
-		n = n - (n / d) * d;
-		d = d / 10;
-		ii++;
-	}
-
-	if (i == 0)
-		_putchar('0' + n);
-
+	_putchar((x % 10) + 48);
 }
